59c1c0501310bdb9a85b1da263ef11669dc29b07
nicsylvia <nicsylvia15f@gmail.com>
2023-05-22 17:17:21 -0700

    working on scrolling

--- src/pages/LandingPage/UpcomingMatches.tsx
+++ src/pages/LandingPage/UpcomingMatches.tsx
@@ -4,24 +4,26 @@ import dhaor from "../../assets/dhoar.png";
 
 const UpcomingMatches = () => {
   return (
-    <div className="bg-UpcomingHero bg-black w-full bg-no-repeat bg-auto bg-center flex justify-center flex-col items-center h-[400px] font-mono">
-      <h1 className="text-white text-4xl ">Upcoming Match</h1>
+    <div className="overflow-hidden">
+      <div className="bg-UpcomingHero w-full bg-no-repeat bg-cover bg-center flex justify-center flex-col items-center h-[500px] font-mono">
+        <h1 className="text-white text-5xl mb-5">Upcoming Match</h1>
 
-      <div className="flex justify-between items-center mt-5 ">
-        <div className="flex justify-center items-center flex-col">
-          <img src={kapa} alt="" />
-          <h3 className="text-white text-lg mt-3">KAPA CLUB</h3>
-        </div>
+        <div className="flex justify-between items-center mt-5 ">
+          <div className="flex justify-center items-center flex-col">
+            <img src={kapa} alt="" />
+            <h3 className="text-white text-lg mt-3">KAPA CLUB</h3>
+          </div>
 
-        <div className="text-white mr-20 ml-20 flex items-center justify-center flex-col">
-          <h3 className="text-lg mb-10">JUNE 16, 17:00</h3>
-          <h1 className="text-[#FBC02D] text-4xl">VS</h1>
-          <h3 className="text-lg mt-10">CENTRAL OLYMPIC STADIUM</h3>
-        </div>
+          <div className="text-white mr-20 ml-20 flex items-center justify-center flex-col">
+            <h3 className="text-lg mb-10">JUNE 16, 17:00</h3>
+            <h1 className="text-[#FBC02D] text-4xl">VS</h1>
+            <h3 className="text-lg mt-10">CENTRAL OLYMPIC STADIUM</h3>
+          </div>
 
-        <div className="flex justify-center items-center flex-col">
-          <img src={dhaor} alt="" />
-          <h3 className="text-white text-lg mt-3">DHAOR CLUB</h3>
+          <div className="flex justify-center items-center flex-col">
+            <img src={dhaor} alt="" />
+            <h3 className="text-white text-lg mt-3">DHAOR CLUB</h3>
+          </div>
         </div>
       </div>
     </div>
