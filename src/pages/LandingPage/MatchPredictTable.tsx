9f15024ee861c9340ac69f3c2bf4153de867af04
nicsylvia <nicsylvia15f@gmail.com>
2023-05-23 17:10:29 -0700

    change the headers

--- src/pages/LandingPage/MatchPredictTable.tsx
+++ src/pages/LandingPage/MatchPredictTable.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MatchPredictTable = () => {
   return (
-    <div>
+    <div className="bg-orange-500">
       <h2 className="text-black text-[23px] font-[500]">Match Fixture</h2>
       <div className="flex w-[160px] mb-[10px] justify-center items-center">
         <div className="w-[50%] h-1 rou bg-[#fbc02d]"></div>
