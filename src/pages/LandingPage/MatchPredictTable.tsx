d8bf140af2e9157b7c76dfe15fde10ceb01a674b
nicsylvia <nicsylvia15f@gmail.com>
2023-05-25 08:08:57 -0700

    styling table

--- src/pages/LandingPage/MatchPredictTable.tsx
+++ src/pages/LandingPage/MatchPredictTable.tsx
@@ -9,12 +9,16 @@ const MatchPredictTable = () => {
         <div className="w-[50%] h-1 bg-[gray]"></div>
       </div>
 
-      <div className="w-[600px] h-[400px] bg-green-500">
-        <div>
-          <h2>NETTROCK FC VS BAMMA FC</h2>
-          <h2>June 16, 17:00</h2>
-          <h2>NETRO STADIUM</h2>
-          <h2>Predict Matches</h2>
+      <div className="w-full py-10 bg-green-500 flex justify-center items-center">
+        <div className="w-[90%] flex bg-slate-500 justify-center items-center flex-col">
+          <div className="flex bg-[gray] py-4 justify-between items-center">
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d]">VS</span> BAMMA FC
+            </h2>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+            <h2>NETRO STADIUM</h2>
+            <h2 className="text-[#fbc02d]">Predict Matches</h2>
+          </div>
         </div>
       </div>
     </div>
