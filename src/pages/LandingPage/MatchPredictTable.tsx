80ec2b5c3142e45839f341bca079e0610d3b31c4
nicsylvia <nicsylvia15f@gmail.com>
2023-05-27 04:16:52 -0700

    saved complete table structure

--- src/pages/LandingPage/MatchPredictTable.tsx
+++ src/pages/LandingPage/MatchPredictTable.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const MatchPredictTable = () => {
   return (
-    <div className="bg-orange-500">
-      <h2 className="text-black text-[23px] font-[500]">Match Fixture</h2>
-      <div className="flex w-[160px] mb-[10px] justify-center items-center">
-        <div className="w-[50%] h-1 rou bg-[#fbc02d]"></div>
-        <div className="w-[50%] h-1 bg-[gray]"></div>
+    <div className="">
+      <div className="mb-7">
+        <h2 className="text-black text-[23px] font-[500]">Match Fixture</h2>
+        <div className="flex w-[160px] mb-[10px] justify-center items-center">
+          <div className="w-[50%] h-1 rou bg-[#fbc02d]"></div>
+          <div className="w-[50%] h-1 bg-[gray]"></div>
+        </div>
       </div>
 
-      <div className="w-[700px] py-10 bg-green-500 flex justify-center flex-col items-center">
-        <div className=" text-white text-base font-bold flex mb-3 w-full bg-[gray] mt-3 px-3 py-4 justify-between items-center">
+      <div className="w-[700px] bg-matchtableBG py-10 [&>*:nth-child(2)]:bg-[#1E1D1B] flex justify-center flex-col items-center">
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full px-3 py-4 justify-between items-center">
           <div>
             <h2>
               NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
@@ -28,13 +30,124 @@ const MatchPredictTable = () => {
           </div>
         </div>
 
-        <div className="flex mb-3 w-[80%] bg-[gray] px-3 py-4 justify-between items-center">
-          <h2>
-            NETTROCK FC <span className="text-[#fbc02d]">VS</span> BAMMA FC
-          </h2>
-          <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
-          <h2>NETRO STADIUM</h2>
-          <h2 className="text-[#fbc02d] ">Predict Matches</h2>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
+        </div>
+        <div className=" text-white text-base font-bold flex mb-3 pt-4 pb-4 w-full  px-3 py-4 justify-between items-center">
+          <div>
+            <h2>
+              NETTROCK FC <span className="text-[#fbc02d] ml-3 mr-3">VS</span>{" "}
+              BAMMA FC
+            </h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d]">June 16, 17:00</h2>
+          </div>
+          <div>
+            <h2>NETRO STADIUM</h2>
+          </div>
+          <div>
+            <h2 className="text-[#fbc02d] cursor-pointer">Predict Matches</h2>
+          </div>
         </div>
       </div>
     </div>
