5fb59c4cdecedd6693a5301ff7736c6f804b2b2a
nicsylvia <nicsylvia15f@gmail.com>
2023-05-29 05:06:13 -0700

    saved champions award

--- src/pages/LandingPage/ChampionAwards.tsx
+++ src/pages/LandingPage/ChampionAwards.tsx
@@ -5,17 +5,17 @@ import cup3 from "../../assets/cup3.png";
 
 const ChampionAwards = () => {
   return (
-    <div className=" mt-10 w-full h-40 bg-no-repeat bg-cover bg-blend-overlay bg-[#23262C] flex  items-center justify-center bg-awardsBG">
-      <div className="w-[85%] flex flex-col h-32">
-        <div>
+    <div className=" mt-10 w-full py-10 bg-no-repeat bg-cover bg-blend-overlay bg-[#23262C] flex  items-center justify-center bg-awardsBG">
+      <div className="w-[85%] py-5 flex flex-col">
+        <div className="mb-5">
           <h2 className="text-white text-[23px] font-[500]">Champion Awards</h2>
-          <div className="flex w-[160px] mb-[10px] justify-center items-center">
+          <div className="flex w-[190px] mb-[10px] justify-center items-center">
             <div className="w-[50%] h-1 rou bg-[#fbc02d]"></div>
             <div className="w-[50%] h-1 bg-[gray]"></div>
           </div>
         </div>
 
-        <div>
+        <div className="flex justify-between items-center">
           <img src={cup1} alt="" />
           <img src={cup2} alt="" />
           <img src={cup3} alt="" />
