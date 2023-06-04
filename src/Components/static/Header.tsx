ca9d448b35dd3d2356c356b85d75499ce47e788b
nicsylvia <nicsylvia15f@gmail.com>
2023-06-04 02:42:39 -0700

    saved all

--- src/Components/static/Header.tsx
+++ src/Components/static/Header.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { ButtonProps } from "../../common";
 import { Link } from "react-scroll";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(true);
+
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div
       className="bg-[#181c25] font-mono flex items-center justify-center w-full h-20
@@ -15,8 +22,7 @@ const Header = () => {
         <div className="w-[60px] h-[60px] object-contain cursor-pointer">
           <img src={logo} alt="" />
         </div>
-
-        <div className="flex justify-between items-center cursor-pointer text-white">
+        <div className="hidden sm:flex justify-between items-center cursor-pointer text-white">
           <Link offset={-100} smooth={true} duration={500} to="Hero">
             <p className="mr-5 text-lg hover:text-[#FBC02D]">Home</p>
           </Link>
@@ -44,8 +50,7 @@ const Header = () => {
             <p className="mr-5 text-lg hover:text-[#FBC02D]">Contact</p>
           </Link>
         </div>
-
-        <div className="flex justify-between items-center">
+        <div className="hidden sm:flex justify-between items-center">
           <ButtonProps
             className="px-4 py-2 mr-5 cursor-pointer rounded-md bg-[#FBC02D]  hover:bg-transparent hover:border hover:border-[#FBC02D] text-white transition-all duration-100"
             text="Sign Up"
@@ -55,6 +60,57 @@ const Header = () => {
             text="Login"
           />
         </div>
+        {/* *************************** */}
+        className=
+        {toggle
+          ? "bg-purple-500 text-white fixed w-[70%] pl-4 top-0 mt-20 duration-300 transition-all z-[-2] md:hidden "
+          : "bg-purple-500 text-white fixed w-[70%] pl-4 top-[-100%] mt-20 duration-300 transition-all  z-[-2] md:hidden"}
+        <div className="text-white text-2xl md:hidden" onClick={toggleMenu}>
+          {toggle ? <AiOutlineMenu /> : <AiOutlineClose />}
+        </div>
+        {/* <div
+          className={
+            toggle
+              ? "hidden bg-[#FBC02D] sm:flex justify-between items-center cursor-pointer text-white"
+              : ""
+          }>
+          <Link offset={-100} smooth={true} duration={500} to="Hero">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">Home</p>
+          </Link>
+
+          <Link offset={-100} smooth={true} duration={500} to="UpcomingMatches">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">Matches</p>
+          </Link>
+
+          <Link offset={-100} smooth={true} duration={500} to="MatchTable">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">
+              Fixtures & Predict
+            </p>
+          </Link>
+          <Link
+            offset={-100}
+            smooth={true}
+            duration={500}
+            to="LatestAndResultWrapper">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">Results</p>
+          </Link>
+          <Link offset={-100} smooth={true} duration={500} to="matchGallery">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">Gallery</p>
+          </Link>
+          <Link offset={-100} smooth={true} duration={500} to="contact">
+            <p className="mr-5 text-lg hover:text-[#FBC02D]">Contact</p>
+          </Link>
+        </div>
+        <div className="hidden sm:flex justify-between items-center">
+          <ButtonProps
+            className="px-4 py-2 mr-5 cursor-pointer rounded-md bg-[#FBC02D]  hover:bg-transparent hover:border hover:border-[#FBC02D] text-white transition-all duration-100"
+            text="Sign Up"
+          />
+          <ButtonProps
+            className="px-4 py-2 cursor-pointer rounded-md border border-[#FBC02D] text-[#FBC02D] hover:bg-[#FBC02D] hover:text-white"
+            text="Login"
+          />
+        </div> */}
       </div>
     </div>
   );
