1d1c2e06f2902790a7018089c76c42f780379ce8
nicsylvia <nicsylvia15f@gmail.com>
2023-06-15 14:30:38 -0700

    saved new updates

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -6,7 +6,7 @@ const SignUp = () => {
       <div className="w-[85%] h-[85%] bg-white flex">
         <div className="w-[50%] h-[100%] bg-red-500"></div>
         <div className="w-[50%] h-[100%] bg-yellow-500 flex items-center justify-center">
-          <div className="w-[95%] h-[95%] rounded-xl bg-signupBG"></div>
+          <div className="w-[95%] h-[95%] rounded-xl bg-awardsBG"></div>
         </div>
       </div>
     </div>
